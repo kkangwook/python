1653a15e38b6ce52e5cd50073035d9f90622ddd8
kkangwook <send2woohyun@naver.com>
2024-09-23 13:42:44 +0900

    Update 2-문자열자료형.py

--- 공부/2-문자열자료형.py
+++ 공부/2-문자열자료형.py
@@ -39,7 +39,10 @@ print(f'i\'m {age}years old and {age+1} next year')
 #count find index
 print(d.count('i'))
 print(d.find('i'))
-print(d.index('i'))
+index=d.index('i')    #제일 처음 나오는 i위치
+print(index)
+index=d.index('i',index+1)    #전에 찾은 i 다음 나오는 i 위치
+print(index)
 
 #join:리스트(문자열)를 문자열로, split:문자열을 리스트로
 print(','.join('abcde'))
